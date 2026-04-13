f66d0370ab93edd99cfdd18a1ee411392ddee7ba
Aryan-202 <aryanvishwakarma275@gmail.com>
2026-04-13 11:41:49 +0530

    feat: add `server.ts` and `app.ts`

---
+++ server/src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import type { Express } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+export default app;

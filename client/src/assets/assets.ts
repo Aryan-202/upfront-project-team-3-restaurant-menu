8664555d5224d7eae1ebf602662ebdd73b7c6e7d
Aryan-202 <aryanvishwakarma275@gmail.com>
2026-04-29 18:41:18 +0530

    @JASWITHA-ATHIPATLA told to delete

--- client/src/assets/assets.ts
+++
@@ -1,9 +0,0 @@
-import spicy_chicken_wings from "./Spicy Chicken Wings.png"
-import avacado_toast from "./Avocado Toast.png"
-
-export const assets = {
-    menu : {
-        spicy_chicken_wings: spicy_chicken_wings,
-        avacado_toast: avacado_toast,
-    }
-}
